fe60c77407ebea47f64f1382cf9817b6931ec568
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-05-31 19:49:42 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter rows: ");
+    scanf("%d", &N);
+
+    // Print the first upper half
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%d", j);
+        }
+        for(j=i-1; j>=1; j--)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    // Print the lower half of the pattern
+    for(i=N-1; i>=1; i--)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%d", j);
+        }
+        for(j=i-1; j>=1; j--)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
